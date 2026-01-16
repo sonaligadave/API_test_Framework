baa0afccbc8b8993c8878ce5000b9ab7a4e9c199
sp <gadave.sonali22@gmail.com>
2026-01-15 23:47:57 -0800

    test framwork push

---
+++ src/test/java/api/endpoints/routes.java
@@ -0,0 +1,14 @@
+package api.endpoints;
+
+/*
+
+ */
+public class routes {
+
+    public static String base_url = " 'https://petstore3.swagger.io/api/v3";
+
+    //User module
+    public static String post_url = base_url + "/user";
+    public static String get_url = base_url + "/user/{username}";
+
+}
